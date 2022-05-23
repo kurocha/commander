4b9fc4c5e74770a73e50d37c9eb2191debc00c09
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2022-05-23 19:42:41 +1200

    Update teapot.rb for teapot 3.x.

--- teapot.rb
+++ teapot.rb
@@ -1,74 +1,67 @@
-# Teapot v2.0.0 configuration generated at 2017-08-19 22:21:11 +1200
 
-required_version "2.0"
+#
+#  This file is part of the "Teapot" project, and is released under the MIT license.
+#
+
+teapot_version "3.0"
 
 # Project Metadata
 
 define_project "commander" do |project|
-	project.title = "Commander"
-	project.summary = 'A command line parser and model.'
+	project.title = 'Commander'
+	project.summary = 'Abstractions for managing blocks of data.'
 	
-	project.license = 'MIT License'
+	project.license = "MIT License"
 	
 	project.add_author 'Samuel Williams', email: 'samuel.williams@oriontransfer.co.nz'
 	
-	project.version = '0.1.0'
+	project.version = "1.0.0"
 end
 
 # Build Targets
 
 define_target 'commander-library' do |target|
-	target.build do
+	target.depends "Language/C++14"
+	
+	target.provides "Library/Commander" do
 		source_root = target.package.path + 'source'
-		copy headers: source_root.glob('Commander/**/*.{h,hpp}')
-		build static_library: 'Commander', source_files: source_root.glob('Commander/**/*.cpp')
-	end
-	
-	target.depends 'Build/Files'
-	target.depends 'Build/Clang'
-	
-	target.depends :platform
-	target.depends 'Language/C++14', private: true
-	
-	target.provides 'Library/Commander' do
-		append linkflags [
-			->{install_prefix + 'lib/libCommander.a'},
-		]
+		
+		library_path = build static_library: "Commander", source_files: source_root.glob('Commander/**/*.cpp')
+		
+		append linkflags library_path
+		append header_search_paths source_root
 	end
 end
 
-define_target 'commander-test' do |target|
-	target.build do |*arguments|
+define_target "commander-tests" do |target|
+	target.depends "Library/UnitTest"
+	
+	target.depends "Language/C++14"
+	target.depends "Library/Commander"
+	
+	target.provides "Test/Commander" do |*arguments|
 		test_root = target.package.path + 'test'
 		
-		run tests: 'Commander', source_files: test_root.glob('Commander/**/*.cpp'), arguments: arguments
+		run source_files: test_root.glob('Commander/**/*.cpp'), arguments: arguments
 	end
-	
-	target.depends 'Language/C++14', private: true
-	
-	target.depends 'Library/UnitTest'
-	target.depends 'Library/Commander'
-	
-	target.provides 'Test/Commander'
 end
 
 # Configurations
 
-define_configuration 'development' do |configuration|
+define_configuration "development" do |configuration|
 	configuration[:source] = "https://github.com/kurocha"
-	configuration.import "commander"
 	
 	# Provides all the build related infrastructure:
-	configuration.require 'platforms'
+	configuration.require "platforms"
+	configuration.require "build-files"
 	
 	# Provides unit testing infrastructure and generators:
-	configuration.require 'unit-test'
+	configuration.require "unit-test"
 	
 	# Provides some useful C++ generators:
-	configuration.require 'generate-cpp-class'
-	
-	configuration.require "generate-project"
-	configuration.require "generate-travis"
+	configuration.require 'generate-project'
+	configuration.require 'generate-travis'
+	configuration.require "generate-cpp-class"
 end
 
 define_configuration "commander" do |configuration|
